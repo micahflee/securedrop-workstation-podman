da3585403dd0d91141f38d033c5bafb11c919f8f
Micah Lee <micah@micahflee.com>
2024-07-31 18:57:52 -0400

    Run the securedrop-client container and pass in the correct environment variables

--- securedrop_workstation_podman/__init__.py
+++ securedrop_workstation_podman/__init__.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
+import json
+import time
 
 
 def main():
@@ -19,15 +21,18 @@ def main():
     except subprocess.CalledProcessError:
         pass
 
-    # Kill securedrop-tor if it's running
+    # Load config
+    with open("config.json") as f:
+        config = json.load(f)
+
+    # Run securedrop-tor
     try:
         subprocess.run(
             ["podman", "kill", "securedrop-tor"], check=True, stdout=subprocess.PIPE
         )
+        time.sleep(0.2)
     except subprocess.CalledProcessError:
         pass
-
-    # Run securedrop-tor
     subprocess.run(
         [
             "podman",
@@ -35,8 +40,41 @@ def main():
             "--name=securedrop-tor",
             "--network=securedrop",
             "--rm",
+            "-e", f"HIDSERV_HOSTNAME={config['hidserv']['hostname']}",
+            "-e", f"HIDSERV_KEY={config['hidserv']['key']}",
             "-d",
             "securedrop/tor",
+        ],
+        check=True,
+    )
+
+    # Run securedrop-client
+    try:
+        subprocess.run(
+            ["podman", "kill", "securedrop-client"], check=True, stdout=subprocess.PIPE
+        )
+        time.sleep(0.2)
+    except subprocess.CalledProcessError:
+        pass
+    subprocess.run(
+        [
+            "podman",
+            "run",
+            "--name=securedrop-client",
+            "--network=securedrop",
+            "--rm",
+            "-d",
+            # Persistent data
+            "-v", f"{os.getcwd()}/volumes/securedrop-client:/sdc-home",
+            # Environment variables for securedrop-client
+            "-e", f"SD_PROXY_ORIGIN=http://{config['hidserv']['hostname']}",
+            # Tell the client it's running in podman
+            "-e", "SDW_PODMAN=1",
+            # Use wayland
+            "-v", f"{os.environ.get('XDG_RUNTIME_DIR')}/{os.environ.get('WAYLAND_DISPLAY')}:/tmp/{os.environ.get('WAYLAND_DISPLAY')}",
+            "-e", "XDG_RUNTIME_DIR=/tmp",
+            "-e", f"WAYLAND_DISPLAY={os.environ.get('WAYLAND_DISPLAY')}",
+            "securedrop/client"
         ],
         check=True,
     )
