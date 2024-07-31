c305ef7048a4a38d71d2943c26f00d69b4af7213
Micah Lee <micah@micahflee.com>
2024-07-31 18:20:36 -0400

    Make securedrop/client container

--- securedrop_workstation_podman/__init__.py
+++ securedrop_workstation_podman/__init__.py
@@ -1,17 +1,23 @@
 import subprocess
+import os
 
 
 def main():
-    # Build the containers
-    print("Building securedrop/tor")
+    # Build containers
     subprocess.run(["podman", "build", "-t", "securedrop/tor", "tor/"], check=True)
+    subprocess.run(["podman", "build", "-t", "securedrop/client", "-f", "./Dockerfile-client"], check=True)
 
-    # Create the securedrop podman network, if it doesn't exist
+    # Make sure volume folders exist
+    if not os.path.exists("volumes"):
+        os.mkdir("volumes")
+    if not os.path.exists("volumes/securedrop-client"):
+        os.mkdir("volumes/securedrop-client")
+
+    # Create the securedrop podman network
     try:
         subprocess.run(["podman", "network", "create", "securedrop"], check=True)
     except subprocess.CalledProcessError:
         pass
-    print("Network 'securedrop' created")
 
     # Kill securedrop-tor if it's running
     try:
@@ -34,3 +40,13 @@ def main():
         ],
         check=True,
     )
+
+"""
+podman run --rm \
+-v $(pwd)/volumes/securedrop-client:/sdc-home \
+-e SDW_PODMAN=1 \
+-v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY \
+-e XDG_RUNTIME_DIR=/tmp \
+-e WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+securedrop/client
+"""
